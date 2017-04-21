404e6d2edda2ec9ad02fb28c7affeaf4ed043e75
Tin Benjamin Matuka <mail@tbmatuka.com>
2017-04-21 09:42:43 +0200

    Added more asserts to controller tests for better coverage

--- tests/src/Controller/SwaggerControllerTest.php
+++ tests/src/Controller/SwaggerControllerTest.php
@@ -25,6 +25,9 @@ class SwaggerControllerTest extends WebTestCase
         $content = json_decode($client->getResponse()->getContent(), true);
         $this->assertTrue(is_array($content));
         $this->assertEquals('testhost', $content['host']);
+        $this->assertEquals('/base', $content['basePath']);
+        $this->assertTrue(in_array('http', $content['schemes']));
+        $this->assertTrue(in_array('https', $content['schemes']));
     }
 
     public function testAlternativeSchema()
@@ -38,5 +41,8 @@ class SwaggerControllerTest extends WebTestCase
         $content = json_decode($client->getResponse()->getContent(), true);
         $this->assertTrue(is_array($content));
         $this->assertEquals('productionhost', $content['host']);
+        $this->assertEquals('/api', $content['basePath']);
+        $this->assertFalse(in_array('http', $content['schemes']));
+        $this->assertTrue(in_array('https', $content['schemes']));
     }
 }
